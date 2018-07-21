829e9ada0b7b14e4863716cdc5a4dc22d7c7bfa4
MasaKuuuu <masaki_kuyama@yahoo.co.jp>
2018-07-21 21:48:28 +0900

    fix ConfirmController

--- app/Http/Controllers/ConfirmCotroller.php
+++ app/Http/Controllers/ConfirmCotroller.php
@@ -6,8 +6,17 @@ use Illuminate\Http\Request;
 
 class ConfirmCotroller extends Controller
 {
-  public function index(){
-    $data = ['msg'=>'これはBladeを利用したサンプルです。',];
-    return view('home.index',$data);
+  public function confirm(Request $request){
+    $param=[
+      'title' => $request->title,
+      'url_key' => str_random(40);
+      'item1' => $request->itme1,
+      'item2' => $request->itme1,
+      'item3' => $request->itme1,
+      'item4' => $request->itme1,
+    ]
+    DB::insert('insert into t_question (title, url_key, itme1, itme2, itme3, itme4) values
+    (:title, :url_key, :itme1, :itme2, :itme3, :itme4)', $param);
+    return view('home.index');
   }
 }
