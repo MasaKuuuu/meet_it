10ddb679ccb6b795de8db8ec48bd51c069054f09
MasaKuuuu <masaki_kuyama@yahoo.co.jp>
2018-07-21 18:05:22 +0900

    git ignore test

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,5 +6,14 @@
   <h1>Blade/Index</h1>
   <p>{{$msg}}</p>
   <p>どんどんここに追加していきます</p>
+  <p>差分の確認はできるのか？</p>
+  <p>エラーが止まった</p>
+  <$php
+  <?php echo $var
+  preg_match();
+
+  ?>
+  ?>
+
 </body>
 </html>
